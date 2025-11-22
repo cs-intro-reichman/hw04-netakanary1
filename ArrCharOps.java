99849f8adb62dbbf6e71cfd76a094000783b6cee
נטע כנרי <netakanary@MacBook-Pro-sl-nt.local>
2025-11-22 21:57:49 +0200

    arr char op7

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -184,8 +184,9 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null)
+        if (str1 == null || str2 == null || str1.length() == 0 || str2.length() == 0)
             return -2;
+
         int minLen = Math.min(str1.length(), str2.length());
 
         for (int i = 0; i < minLen; i++) {
